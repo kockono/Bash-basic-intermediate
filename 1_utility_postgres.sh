65ff90f7a60103cfe77569b38163aedb3e1738b0
kockono <kockonoop@gmail.com>
2022-08-04 16:05:19 -0600

    utilities

---
+++ 1_utility_postgres.sh
@@ -0,0 +1,14 @@
+# !/bin/bash
+# Programa para realizar operaciones en Postgres
+# -rw-rw-r-- User-Group-OtherUser
+
+
+echo "Script para backup de Postgres"
+echo This is a sample text file > sample.txt
+
+
+(
+echo Here is my first line
+echo Here is my second line
+echo Here is my third line
+)>"myNewTextFile.txt"
