a308dc4ff63a31caa4adfd2e98d3f04114080308
kockono <kockonoop@gmail.com>
2022-08-05 14:58:47 -0600

    export var

--- 1_utility_postgres.sh
+++ 1_utility_postgres.sh
@@ -2,13 +2,6 @@
 # Programa para realizar operaciones en Postgres
 # -rw-rw-r-- User-Group-OtherUser
 
-
-echo "Script para backup de Postgres"
-echo This is a sample text file > sample.txt
-
-
-(
-echo Here is my first line
-echo Here is my second line
-echo Here is my third line
-)>"myNewTextFile.txt"
+#Variables de Entorno S.O
+COURSE_NAME=Progrmacion bash
+export COURSE_NAME
