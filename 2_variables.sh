ec2f48b18e6e919d1e00fe1f54a5e44d69ad812c
kockono <kockonoop@gmail.com>
2022-08-09 15:56:04 -0600

    import script

--- 2_variables.sh
+++ 2_variables.sh
@@ -2,6 +2,10 @@
 # Programa para revisar declaración de variables
 
 opcion=0
-nombre=Marco
+nombre=kokiwis
 
 echo "$nombre y $opcion"
+
+export nombre
+# LLamar al siguiente script para recuperar la variable
+./3_import_script.sh
---
+++ 3_import_script.sh
@@ -0,0 +1,5 @@
+# !/bin/bash
+# Programa para revisar declaración de variables
+
+# Se debe mandar desde 2_variables.sh y correr este script ./3_import_script.sh
+echo "Opcion del nombre pasado del script anterior: $nombre"
