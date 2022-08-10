2185864017f0dcea0d9af4ffbaf85eb989893bd0
kockono <kockonoop@gmail.com>
2022-08-10 16:01:17 -0600

    Argumentos

--- 3_import_script.sh
+++ 3_import_script.sh
@@ -2,4 +2,7 @@
 # Programa para revisar declaración de variables
 
 # Se debe mandar desde 2_variables.sh y correr este script ./3_import_script.sh
+
+
+
 echo "Opcion del nombre pasado del script anterior: $nombre"
--- 5_argumentos.sh
+++ 5_argumentos.sh
@@ -1,7 +1,14 @@
 # !/bin/bash
 # Programa para ejemplificar el paso de argumentos
 
+# $# El numero de parametros enviados
+# $* Los parametros enviados
+
 nombreCurso=$1
 horarioCurso=$2
 
-echo "El nombre del curso es: $ %nombreCurso"
+echo "El nombre del curso es: $nombreCurso"
+echo "El nombre del curso es: $horarioCurso"
+
+echo "El número de parámetros enviados es: $#"
+echo "El número de parámetros enviados son: $*"
